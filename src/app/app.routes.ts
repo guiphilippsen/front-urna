1b3ba82c0b893f140641578e8fdc710bd2f6e88e
Guilherme Souza <guilhermephilippsen2003@gmail.com>
2024-10-25 16:51:30 -0300

    commit misterioso

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { EleitoresComponent } from './component/eleitores/eleitores.component';
+import { CandidatosComponent } from './component/candidatos/candidatos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'eleitores', component: EleitoresComponent },
+  { path: 'candidatos', component: CandidatosComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/component/eleitores/eleitores.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eleitores',
+  standalone: true,
+  imports: [],
+  templateUrl: './eleitores.component.html',
+  styleUrl: './eleitores.component.scss'
+})
+export class EleitoresComponent {
+
+}
